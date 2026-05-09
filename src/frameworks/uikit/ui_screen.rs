1442765c6d4b977cd7887a8440538848491bf2ee
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-09 10:52:03 -0400

    Update ui_screen.rs

--- src/frameworks/uikit/ui_screen.rs
+++ src/frameworks/uikit/ui_screen.rs
@@ -50,16 +50,16 @@ pub const CLASSES: ClassExports = objc_classes! {
 // MARK: - Geometry
 
 - (CGRect)bounds {
-    // Force the legacy iPhone resolution (320x480)
     CGRect {
         origin: CGPoint { x: 0.0, y: 0.0 },
         size: CGSize {
-            width:  320.0 as CGFloat,
-            height: 480.0 as CGFloat,
+            // Swap these so it's wider than it is tall
+            width:  480.0 as CGFloat, 
+            height: 320.0 as CGFloat,
         },
     }
 }
-    
+     
 - (CGRect)nativeBounds {
     // Same as bounds at scale 1 — we don't model the physical pixel grid.
     msg![env; this bounds]
@@ -108,13 +108,13 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (id)currentMode {
     let size = CGSize {
-        width:  320.0 as CGFloat,
-        height: 480.0 as CGFloat,
+        width:  480.0 as CGFloat,
+        height: 320.0 as CGFloat,
     };
 
     crate::frameworks::uikit::ui_screen_mode::from_size(env, size, 1.0)
 }
-    
+      
 - (id)preferredMode {
     nil
 }
