c7a70683a2a2e82501e7f00dd8cff78aee9a0133
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-09 07:50:24 -0400

    Update media_item_collection.rs

--- src/frameworks/media_player/media_item_collection.rs
+++ src/frameworks/media_player/media_item_collection.rs
@@ -5,14 +5,31 @@
  */
 //! `MPMediaItemCollection`.
 
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{id, objc_classes, ClassExports};
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation MPMediaItemCollection: MPMediaEntity
-// TODO
+
+- (id)initWithItems:(id)items {
+    log!("Gameloft Hack: Initializing dummy MediaItemCollection with provided items.");
+    // Return 'this' to show the object was successfully initialized.
+    this
+}
+
+- (uint32_t)count {
+    // Return 0 so the game thinks the playlist/library is empty.
+    0
+}
+
+- (id)items {
+    // If the game asks for the actual list of songs, we return nil (0).
+    // Since count is 0, the game shouldn't try to read this list.
+    crate::objc::nil
+}
+
 @end
 
 };
