ef2a7704436be306ab4462bdb027d4f0fd769a54
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-10 12:26:31 -0400

    Update ns_objc_runtime.rs

--- src/frameworks/foundation/ns_objc_runtime.rs
+++ src/frameworks/foundation/ns_objc_runtime.rs
@@ -6,11 +6,13 @@ use crate::objc::{id, nil, Class, SEL};
 use crate::Environment;
 
 fn NSStringFromSelector(env: &mut Environment, selector: SEL) -> id {
+    // TODO: caching?
     let string = selector.as_str(&env.mem).to_string();
     ns_string::from_rust_string(env, string)
 }
 
 fn NSSelectorFromString(env: &mut Environment, string: id) -> SEL {
+    // TODO: avoid copy?
     let string = ns_string::to_rust_string(env, string);
     env.objc.register_host_selector(string.into(), &mut env.mem)
 }
@@ -19,6 +21,7 @@ pub fn NSStringFromClass(env: &mut Environment, class: Class) -> id {
     if class.is_null() {
         return nil;
     }
+    // TODO: caching?
     let string = env.objc.get_class_name(class).to_string();
     ns_string::from_rust_string(env, string)
 }
@@ -27,62 +30,15 @@ fn NSClassFromString(env: &mut Environment, string: id) -> Class {
     if string == nil {
         return nil;
     }
-    let class_name = ns_string::to_rust_string(env, string);
+    // TODO: avoid copy?
+    let string = ns_string::to_rust_string(env, string);
 
-    // Using get_class instead of get_known_class to prevent panics
-    // and allow the game to handle missing classes gracefully.
-    match env.objc.get_class(&class_name) {
-        Some(class) => class,
-        None => {
-            println!("NSClassFromString: Class '{}' not found, returning nil", class_name);
-            nil
-        }
-    }
-}
-
-/// Internal helper to handle the logic for setting Objective-C properties.
-fn perform_set_property(
-    env: &mut Environment,
-    _self: id,
-    _cmd: SEL,
-    new_value: id,
-    offset: u32,
-) {
-    let ptr = _self + offset;
-    
-    // Read old value (to release it later)
-    let old_value: u32 = env.mem.read(ptr).unwrap_or(0);
-
-    let retain_sel = env.objc.sel_retain;
-    let release_sel = env.objc.sel_release;
-
-    // Retain new value
-    if new_value != nil {
-        env.objc.msg_send(new_value, retain_sel, &[], &mut env.mem);
-    }
-
-    // Store new value in the object's memory
-    if let Err(_) = env.mem.write(ptr, new_value) {
-        println!("Failed to write property at offset {}", offset);
-    }
-
-    // Release old value
-    if old_value != nil {
-        env.objc.msg_send(old_value, release_sel, &[], &mut env.mem);
-    }
-}
-
-// These wrappers make the functions available to the export macro
-fn _objc_setProperty_nonatomic_copy(env: &mut Environment, _self: id, _cmd: SEL, new_value: id, offset: u32) {
-    perform_set_property(env, _self, _cmd, new_value, offset);
-}
-
-fn objc_setProperty(env: &mut Environment, _self: id, _cmd: SEL, new_value: id, offset: u32) {
-    perform_set_property(env, _self, _cmd, new_value, offset);
-}
-
-fn objc_setProperty_atomic(env: &mut Environment, _self: id, _cmd: SEL, new_value: id, offset: u32) {
-    perform_set_property(env, _self, _cmd, new_value, offset);
+    // While this method is supposed to return nil if the class is not found,
+    // touchHLE is missing many classes that apps might expect to be present,
+    // so this could be troublesome. So, let's use get_known_class, which panics
+    // when it can't find the class. We could except certain classes or apps if
+    // we need to.
+    env.objc.get_known_class(&string, &mut env.mem)
 }
 
 pub const FUNCTIONS: FunctionExports = &[
@@ -90,7 +46,4 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(NSSelectorFromString(_)),
     export_c_func!(NSClassFromString(_)),
     export_c_func!(NSStringFromClass(_)),
-    export_c_func!(_objc_setProperty_nonatomic_copy(id, SEL, id, u32)),
-    export_c_func!(objc_setProperty(id, SEL, id, u32)),
-    export_c_func!(objc_setProperty_atomic(id, SEL, id, u32)),
 ];
