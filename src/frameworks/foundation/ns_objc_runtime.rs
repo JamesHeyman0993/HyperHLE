da9b1529c0b7f8f7f4e44e46107268c6c0230c57
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-10 14:31:57 -0400

    Update ns_objc_runtime.rs

--- src/frameworks/foundation/ns_objc_runtime.rs
+++ src/frameworks/foundation/ns_objc_runtime.rs
@@ -6,11 +6,13 @@ use crate::objc::{id, nil, Class, SEL};
 use crate::Environment;
 
 fn NSStringFromSelector(env: &mut Environment, selector: SEL) -> id {
+    // TODO: caching?
     let string = selector.as_str(&env.mem).to_string();
     ns_string::from_rust_string(env, string)
 }
 
 fn NSSelectorFromString(env: &mut Environment, string: id) -> SEL {
+    // TODO: avoid copy?
     let string = ns_string::to_rust_string(env, string);
     env.objc.register_host_selector(string.into(), &mut env.mem)
 }
@@ -19,6 +21,7 @@ pub fn NSStringFromClass(env: &mut Environment, class: Class) -> id {
     if class.is_null() {
         return nil;
     }
+    // TODO: caching?
     let string = env.objc.get_class_name(class).to_string();
     ns_string::from_rust_string(env, string)
 }
@@ -27,30 +30,15 @@ fn NSClassFromString(env: &mut Environment, string: id) -> Class {
     if string == nil {
         return nil;
     }
-    let name = ns_string::to_rust_string(env, string);
+    // TODO: avoid copy?
+    let string = ns_string::to_rust_string(env, string);
 
-    match env.objc.get_class(&name) {
-        Some(class) => class,
-        None => nil,
-    }
-}
-
-/// The actual logic for setting properties
-fn objc_setProperty(env: &mut Environment, _self: id, _cmd: SEL, val: id, offset: u32) {
-    let ptr = _self + offset;
-    if let Err(_) = env.mem.write(ptr, val) {
-        log::error!("objc_setProperty failed at offset {}", offset);
-    }
-}
-
-/// Wrapper for the nonatomic copy version
-fn _objc_setProperty_nonatomic_copy(env: &mut Environment, _self: id, _cmd: SEL, val: id, offset: u32) {
-    objc_setProperty(env, _self, _cmd, val, offset);
-}
-
-/// Wrapper for the atomic version
-fn objc_setProperty_atomic(env: &mut Environment, _self: id, _cmd: SEL, val: id, offset: u32) {
-    objc_setProperty(env, _self, _cmd, val, offset);
+    // While this method is supposed to return nil if the class is not found,
+    // touchHLE is missing many classes that apps might expect to be present,
+    // so this could be troublesome. So, let's use get_known_class, which panics
+    // when it can't find the class. We could except certain classes or apps if
+    // we need to.
+    env.objc.get_known_class(&string, &mut env.mem)
 }
 
 pub const FUNCTIONS: FunctionExports = &[
@@ -58,7 +46,4 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(NSSelectorFromString(_)),
     export_c_func!(NSClassFromString(_)),
     export_c_func!(NSStringFromClass(_)),
-    export_c_func!(objc_setProperty(id, SEL, id, u32)),
-    export_c_func!(_objc_setProperty_nonatomic_copy(id, SEL, id, u32)),
-    export_c_func!(objc_setProperty_atomic(id, SEL, id, u32)),
 ];
