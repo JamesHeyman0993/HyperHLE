039c1a6330b1746cadd45340524c96ea3fb192c9
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-11 18:48:03 -0400

    Update ui_view.rs

--- src/frameworks/uikit/ui_view.rs
+++ src/frameworks/uikit/ui_view.rs
@@ -292,37 +292,38 @@ pub const CLASSES: ClassExports = objc_classes! {
 + (())animateWithDuration:(f64)_duration animations:(id)animations completion:(id)completion {
     if animations != nil {
         // Read the function pointer at offset 12 (32-bit ABI for blocks)
-        let invoke_addr: u32 = env.mem.read(animations.to_vaddr() + 12u32);
+        let invoke_addr: u32 = env.mem.read(animations.to_bits() + 12u32);
         if invoke_addr != 0 {
-            let _: () = msg_send_no_type_checking(env, (animations, invoke_addr));
+            // We call the block's function pointer directly using its address
+            let _: () = msg_send_no_type_checking(env, (animations, SEL::from_bits(invoke_addr)));
         }
     }
 
     if completion != nil {
-        let invoke_addr: u32 = env.mem.read(completion.to_vaddr() + 12u32);
+        let invoke_addr: u32 = env.mem.read(completion.to_bits() + 12u32);
         if invoke_addr != 0 {
-            // Pass 'true' (1u8 or true) for the 'finished' parameter
-            let _: () = msg_send_no_type_checking(env, (completion, invoke_addr, true));
+            // Blocks expect (block_ptr, finished_bool)
+            let _: () = msg_send_no_type_checking(env, (completion, SEL::from_bits(invoke_addr), true));
         }
     }
 }
 
 + (())animateWithDuration:(f64)_duration delay:(f64)_delay options:(u32)_options animations:(id)animations completion:(id)completion {
     if animations != nil {
-        let invoke_addr: u32 = env.mem.read(animations.to_vaddr() + 12u32);
+        let invoke_addr: u32 = env.mem.read(animations.to_bits() + 12u32);
         if invoke_addr != 0 {
-            let _: () = msg_send_no_type_checking(env, (animations, invoke_addr));
+            let _: () = msg_send_no_type_checking(env, (animations, SEL::from_bits(invoke_addr)));
         }
     }
 
     if completion != nil {
-        let invoke_addr: u32 = env.mem.read(completion.to_vaddr() + 12u32);
+        let invoke_addr: u32 = env.mem.read(completion.to_bits() + 12u32);
         if invoke_addr != 0 {
-            let _: () = msg_send_no_type_checking(env, (completion, invoke_addr, true));
+            let _: () = msg_send_no_type_checking(env, (completion, SEL::from_bits(invoke_addr), true));
         }
     }
 }
-                  
+                     
 + (())_touchHLE_animationDidStopFireMethod:(id)which_timer {
     let dict: id = msg![env; which_timer userInfo];
     let key_delegate: id = get_static_str(env, "_touchHLE_uiview_anim_delegate");
