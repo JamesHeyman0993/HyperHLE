0cda0aaa56a06159837348bb1430d1fa5796cc13
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-11 11:47:24 -0400

    Update ns_objc_runtime.rs

--- src/frameworks/foundation/ns_objc_runtime.rs
+++ src/frameworks/foundation/ns_objc_runtime.rs
@@ -6,13 +6,11 @@ use crate::objc::{id, nil, Class, SEL};
 use crate::Environment;
 
 fn NSStringFromSelector(env: &mut Environment, selector: SEL) -> id {
-    // TODO: caching?
     let string = selector.as_str(&env.mem).to_string();
     ns_string::from_rust_string(env, string)
 }
 
 fn NSSelectorFromString(env: &mut Environment, string: id) -> SEL {
-    // TODO: avoid copy?
     let string = ns_string::to_rust_string(env, string);
     env.objc.register_host_selector(string.into(), &mut env.mem)
 }
@@ -21,7 +19,6 @@ pub fn NSStringFromClass(env: &mut Environment, class: Class) -> id {
     if class.is_null() {
         return nil;
     }
-    // TODO: caching?
     let string = env.objc.get_class_name(class).to_string();
     ns_string::from_rust_string(env, string)
 }
@@ -30,26 +27,20 @@ fn NSClassFromString(env: &mut Environment, string: id) -> Class {
     if string == nil {
         return nil;
     }
-    // TODO: avoid copy?
     let string = ns_string::to_rust_string(env, string);
-
-    // While this method is supposed to return nil if the class is not found,
-    // touchHLE is missing many classes that apps might expect to be present,
-    // so this could be troublesome. So, let's use get_known_class, which panics
-    // when it can't find the class. We could except certain classes or apps if
-    // we need to.
     env.objc.get_known_class(&string, &mut env.mem)
 }
 
 // --- ENGINE GLUE FIXES ---
-// These functions allow the game engine to link its script variables to memory.
-// This is required for the "Start" button and other UI logic to function.
 
 fn objc_setProperty(env: &mut Environment, _self: id, _cmd: SEL, val: id, offset: u32) {
     let dest_ptr = _self + offset;
-    // .write_ref() is the standard way in touchHLE to write a pointer (id)
-    // into a memory address.
-    let _ = env.mem.write_ref(dest_ptr, val);
+    
+    // Fix: Convert the 'id' (pointer) to a u32 integer. 
+    // This allows the generic .write() method to handle it as a 
+    // simple number instead of a complex object.
+    let val_bits: u32 = val.into(); 
+    let _ = env.mem.write(dest_ptr, val_bits);
 }
 
 fn _objc_setProperty_nonatomic_copy(env: &mut Environment, _self: id, _cmd: SEL, val: id, offset: u32) {
@@ -67,7 +58,6 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(NSSelectorFromString(_)),
     export_c_func!(NSClassFromString(_)),
     export_c_func!(NSStringFromClass(_)),
-    // Missing Property Setter exports:
     export_c_func!(objc_setProperty(id, SEL, id, u32)),
     export_c_func!(_objc_setProperty_nonatomic_copy(id, SEL, id, u32)),
     export_c_func!(objc_setProperty_atomic(id, SEL, id, u32)),
