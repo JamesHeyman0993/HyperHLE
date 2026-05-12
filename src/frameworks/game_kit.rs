c063da934550af4a17d8b7d100913dba74f6be62
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-12 17:52:39 -0400

    Update game_kit.rs

--- src/frameworks/game_kit.rs
+++ src/frameworks/game_kit.rs
@@ -5,15 +5,11 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 //! GameKit framework.
-//!
-//! Some features of this framework are only in iOS 4.1+, but some games (like
-//! "Cut the Rope") may use it to check for game center availability with
-//! a `respondsToSelector:` call to some objects of this framework.
-//!
-//! Thus, we need to provide some stubs in order to not crash on that call.
+
+use crate::dyld::{ConstantExports, HostConstant}; // Added for constants
 
 pub mod ad_banner_view;
-pub mod fb_session; // <--- ДОБАВЬ ЭТУ СТРОКУ СЮДА
+pub mod fb_session; 
 pub mod gk_leaderboard_view_controller;
 pub mod gk_local_player;
 mod gk_score;
@@ -25,17 +21,26 @@ pub struct State {
     pub local_player: gk_local_player::State,
 }
 
+// Added to fix _GKErrorDomain crash
+pub const CONSTANTS: ConstantExports = &[
+    ("_GKErrorDomain", HostConstant::NSString("GKErrorDomain")),
+];
+
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
     path: "/System/Library/Frameworks/GameKit.framework/GameKit",
     aliases: &[],
     class_exports: &[
         ad_banner_view::CLASSES,
-        fb_session::CLASSES, // <--- И ДОБАВЬ ЭТУ СТРОКУ СЮДА
+        fb_session::CLASSES, 
         gk_leaderboard_view_controller::CLASSES,
         gk_local_player::CLASSES,
         gk_score::CLASSES,
         gk_session::CLASSES,
     ],
-    constant_exports: &[gk_local_player::CONSTANTS, ad_banner_view::CONSTANTS],
+    constant_exports: &[
+        gk_local_player::CONSTANTS, 
+        ad_banner_view::CONSTANTS,
+        CONSTANTS, // Added our new constants here
+    ],
     function_exports: &[],
 };
