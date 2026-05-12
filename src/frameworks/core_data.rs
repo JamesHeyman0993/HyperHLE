90840014a1f9561023e0c47c641a26e810782cef
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-12 14:14:50 -0400

    Update core_data.rs

--- src/frameworks/core_data.rs
+++ src/frameworks/core_data.rs
@@ -17,21 +17,25 @@ pub const CLASSES: ClassExports = objc_classes! {
     - (id)persistentStoreCoordinator { crate::objc::nil }
     - (id)persistentStore { crate::objc::nil }
     - (bool)save:(id)_error { true }
+    @end
     
     @implementation NSManagedObjectModel : NSObject
     - (id)init { this }
     - (id)initWithContentsOfURL:(id)_url { this }
+    @end
 
     @implementation NSPersistentStoreCoordinator : NSObject
     - (id)initWithManagedObjectModel:(id)_model { this }
     - (id)addPersistentStoreWithType:(id)_type configuration:(id)_config URL:(id)_url options:(id)_options error:(id)_error {
         this 
     }
+    @end
 
     @implementation NSManagedObject : NSObject
     - (id)initWithEntity:(id)_entity insertIntoManagedObjectContext:(id)_context { this }
     - (id)entity { crate::objc::nil }
     - (id)objectID { crate::objc::nil }
+    @end
 
     @implementation NSEntityDescription : NSObject
     + (id)entityForName:(id)_name inManagedObjectContext:(id)_context { crate::objc::nil }
