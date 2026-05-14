19856f9d7c9b47b6181c169177066b6665e9da4f
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-14 06:14:26 -0400

    Update ui_application.rs

--- src/frameworks/uikit/ui_application.rs
+++ src/frameworks/uikit/ui_application.rs
@@ -138,7 +138,7 @@ pub const CLASSES: ClassExports = objc_classes! {
 
 - (UIInterfaceOrientation)statusBarOrientation {
     // Fix for Fast & Furious: Force landscape
-    if let Some(bundle) = env.bundle.as_ref() {
+        if let Some(ref bundle) = env.bundle {
         let bundle_id = bundle.bundle_identifier();
         if bundle_id == "com.iplay.ff63d" {
             return UIDeviceOrientationLandscapeRight;
