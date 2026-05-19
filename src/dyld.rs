3ef6012ddf59627bbab93ebb6903405731ac71a2
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-18 22:17:19 -0400

    Update dyld.rs

--- src/dyld.rs
+++ src/dyld.rs
@@ -652,7 +652,7 @@ impl Dyld {
             return None;
         }
         
-        // --- FIXED CRITICAL MATH INTERCEPT ---
+               // --- FIXED CRITICAL MATH INTERCEPT ---
         let vector_math_mangled = "__ZNSt6vectorIPN5Maths10cMatrix4x4ESaIS2_EE13_M_insert_auxEN9__gnu_cxx17__normal_iteratorIPS2_S4_EERKS2_";
         if symbol == vector_math_mangled {
              log!("HyperHLE: Catching unmapped vector routine: {}. Allocating host adapter bridge.", symbol);
@@ -665,6 +665,19 @@ impl Dyld {
              let idx_f: u32 = (self.linked_host_functions.len() - 1).try_into().unwrap();
              return Some(self.linked_host_functions[idx_f as usize].1);
         }
+
+        // --- START OF NEW GUEST C++ ROUTINE BYPASS ---
+        if symbol == "___dynamic_cast" || symbol.starts_with("__ZNSt11_Deque_base") {
+            log!("HyperHLE: Forcing guest dylib resolution for: {}", symbol);
+            for dylib in bins.iter() {
+                if let Some(&addr) = dylib.exported_symbols.get(symbol) {
+                    let _ = link_by_restoring_stub(mem, cpu, addr, svc_pc, info.entry_size, pic_offset);
+                    return None; // Successfully hooked into the guest standard library!
+                }
+            }
+            log!("HyperHLE Warning: Explicitly bypassed symbol {} was not found in loaded guest binaries.", symbol);
+        }
+        // --- END OF NEW GUEST C++ ROUTINE BYPASS ---
         
         if let Some(&addr) = self.non_lazy_host_functions.get(symbol) {
             let _ = link_by_restoring_stub(mem, cpu, addr.addr_with_thumb_bit(), svc_pc, info.entry_size, pic_offset);
@@ -678,31 +691,8 @@ impl Dyld {
             }
         }
 
-        if let Some(&(symbol, f)) = search_host_dylibs(|d| d.function_exports, symbol) {
-            let idx: u32 = self.linked_host_functions.len().try_into().unwrap();
-            let mut svc = idx + Self::SVC_LINKED_FUNCTIONS_BASE;
-            if info.entry_size == 4 {
-                assert!(svc < Self::SVC_LAZY_LINK_RET_FLAG);
-                svc |= Self::SVC_LAZY_LINK_RET_FLAG;
-            }
-            self.linked_host_functions.push((symbol, f));
-            let stub_function_ptr: MutPtr<u32> = Ptr::from_bits(svc_pc);
-            mem.write(stub_function_ptr, encode_a32_svc(svc));
-            if info.entry_size != 4 {
-                assert!(mem.read(stub_function_ptr + 1) == encode_a32_ret());
-            }
-
-            cpu.invalidate_cache_range(stub_function_ptr.to_bits(), 4);
-            return Some(f);
-        }
-
-        for dylib in bins.iter() {
-            if let Some(&addr) = dylib.exported_symbols.get(symbol) {
-                let _ = link_by_restoring_stub(mem, cpu, addr, svc_pc, info.entry_size, pic_offset);
-                return None;
-            }
-        }
-
+        // ... [Rest of your unmodified code below] ...
+        
         log!("Warning: call to unimplemented function {}; installing return-0 stub", symbol);
         
         let leaked_symbol: &'static str = Box::leak(symbol.to_string().into_boxed_str());
