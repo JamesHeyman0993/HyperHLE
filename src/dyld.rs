58970dc01387d8b53f700ef6f7eabca158f92e45
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-18 21:16:38 -0400

    Update dyld.rs

--- src/dyld.rs
+++ src/dyld.rs
@@ -877,32 +877,33 @@ fn touchhle_vector_matrix_insert_aux(env: &mut Environment, vector_this: u32, po
         env.mem.write(Ptr::from_bits(position_iterator), matrix_ptr_val);
         finish += 4;
         env.mem.write(vec_ptr + 1, finish);
-    } else {
+        } else {
         // Handle array reallocation if storage is maxed out
         let current_size = finish.saturating_sub(start);
         let new_capacity = if current_size == 0 { 4 } else { current_size * 2 };
         
-        let new_start_ptr = env.mem.alloc(new_capacity);
+        let new_start_ptr = (*env.mem).alloc(new_capacity);
         let new_start = new_start_ptr.to_bits();
         
         let prefix_bytes = position_iterator.saturating_sub(start);
         if prefix_bytes > 0 {
             let data = (*env.mem).read_bytes(start, prefix_bytes as usize);
-(*env.mem).write_bytes(new_start, &data);
+            (*env.mem).write_bytes(new_start, &data);
+        }
             
-        env.mem.write(Ptr::from_bits(new_start + prefix_bytes), matrix_ptr_val);
+        (*env.mem).write(Ptr::from_bits(new_start + prefix_bytes), matrix_ptr_val);
         
         let suffix_bytes = finish.saturating_sub(position_iterator);
         if suffix_bytes > 0 {
             let data = (*env.mem).read_bytes(position_iterator, suffix_bytes as usize);
-(*env.mem).write_bytes(new_start + prefix_bytes + 4, &data);
+            (*env.mem).write_bytes(new_start + prefix_bytes + 4, &data);
+        }
             
-        env.mem.write(vec_ptr + 0, new_start);
-        env.mem.write(vec_ptr + 1, new_start + current_size + 4);
-        env.mem.write(vec_ptr + 2, new_start + new_capacity);
+        (*env.mem).write(vec_ptr + 0, new_start);
+        (*env.mem).write(vec_ptr + 1, new_start + current_size + 4);
+        (*env.mem).write(vec_ptr + 2, new_start + new_capacity);
     }
-}
-
+    
 fn touchhle_sqlite3_prepare_v2(_env: &mut Environment, _db: u32, _z_sql: u32, _n_byte: i32, pp_stmt: u32, _pz_tail: u32) -> i32 {
     0 
 }
