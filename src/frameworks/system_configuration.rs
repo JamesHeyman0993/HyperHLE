da28a37f4bf9e06b16ff3ec84d5047560f0d0068
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-24 11:34:01 -0400

    Update system_configuration.rs

--- src/frameworks/system_configuration.rs
+++ src/frameworks/system_configuration.rs
@@ -7,10 +7,17 @@
 
 mod sc_network_reachability;
 
+use crate::dyld::{ConstantExports, HostConstant};
+
+// HyperHLE Patch: Maps the external Wi-Fi initialization key requested by Gameloft titles
+pub const CONSTANTS: ConstantExports = &[
+    ("_kCNNetworkInfoKeySSID", HostConstant::NullPtr),
+];
+
 pub const DYLIB: crate::dyld::HostDylib = crate::dyld::HostDylib {
     path: "/System/Library/Frameworks/SystemConfiguration.framework/SystemConfiguration",
     aliases: &[],
     class_exports: &[sc_network_reachability::CLASSES],
-    constant_exports: &[],
+    constant_exports: &[CONSTANTS], // Registered our new constants table here
     function_exports: &[sc_network_reachability::FUNCTIONS],
 };
