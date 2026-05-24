d405f1779fb2eaabe06d21499c4b53d4cceb2152
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-24 16:02:28 -0400

    Update frameworks.rs

--- src/frameworks.rs
+++ src/frameworks.rs
@@ -35,6 +35,7 @@ pub mod core_text;
 pub mod core_animation;
 pub mod core_audio;
 pub mod core_audio_types;
+pub mod core_bluetooth;
 pub mod core_foundation;
 pub mod core_graphics;
 pub mod core_location;
