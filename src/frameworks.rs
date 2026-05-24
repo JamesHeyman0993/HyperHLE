8bf0e7c1188951c82bb68efbf071b59d19e28759
JamesHeyman0993 <gamingbeast573337@gmail.com>
2026-05-24 16:04:33 -0400

    Update frameworks.rs

--- src/frameworks.rs
+++ src/frameworks.rs
@@ -26,6 +26,7 @@ pub mod address_book;
 pub mod address_book_ui;
 pub mod audio_toolbox;
 pub mod avfoundation;
+pub mod assets_library;
 pub mod carbon_core;
 pub mod cf_network;
 pub mod common_crypto;
